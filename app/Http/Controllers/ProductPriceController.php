b7c919f34b260d73a27511423ca6c740d458ce7d
Ashley Gibson <ashley@nosegraze.com>
2023-04-02 17:13:42 +0100

    Allow some JSON responses

--- app/Http/Controllers/ProductPriceController.php
+++ app/Http/Controllers/ProductPriceController.php
@@ -8,7 +8,9 @@ use App\Http\Requests\UpdateProductPriceRequest;
 use App\Models\Product;
 use App\Models\ProductPrice;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 
 class ProductPriceController extends Controller
 {
@@ -20,44 +22,56 @@ class ProductPriceController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create(Product $product) : View
+    public function create(Product $product): View
     {
         return view('prices.create', [
             'product' => $product,
-            'price' => $product->prices()->make(),
+            'price'   => $product->prices()->make(),
         ]);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreProductPriceRequest $request, Product $product, CreateNewProductPrice $priceCreator) : RedirectResponse
-    {
+    public function store(
+        StoreProductPriceRequest $request,
+        Product $product,
+        CreateNewProductPrice $priceCreator
+    ): RedirectResponse|JsonResponse {
         $price = $priceCreator->createFromRequest($request, $product);
 
-        return redirect()->route('products.show', $product);
+        if ($request->wantsJson()) {
+            return response()->json($price->toArray());
+        } else {
+            $request->session()->flash('status', 'Price created successfully.');
+
+            return redirect()->route('products.show', $product);
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(ProductPrice $productPrice)
+    public function show(ProductPrice $productPrice): View
     {
-        //
+        // @TODO do I want this?
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(ProductPrice $productPrice)
+    public function edit(Product $product, ProductPrice $productPrice): View
     {
-        //
+        return view('prices.edit', [
+            'product' => $product,
+            'price'   => $productPrice,
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateProductPriceRequest $request, ProductPrice $productPrice)
+    public function update(UpdateProductPriceRequest $request, ProductPrice $productPrice): RedirectResponse
     {
         //
     }
@@ -65,8 +79,19 @@ class ProductPriceController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(ProductPrice $productPrice)
-    {
-        //
+    public function destroy(
+        Request $request,
+        Product $product,
+        ProductPrice $productPrice
+    ): RedirectResponse|JsonResponse {
+        $productPrice->delete();
+
+        if ($request->wantsJson()) {
+            return response()->json(null);
+        } else {
+            $request->session()->flash('status', 'Price deleted successfully.');
+
+            return redirect()->route('products.show', $product);
+        }
     }
 }
