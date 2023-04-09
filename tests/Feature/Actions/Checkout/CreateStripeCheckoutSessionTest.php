8d4980196902200c5b35937de0ded03eb3523e80
Ashley Gibson <ashley@nosegraze.com>
2023-04-09 13:39:16 +0100

    Add tests

--- tests/Feature/Actions/Checkout/CreateStripeCheckoutSessionTest.php
+++ tests/Feature/Actions/Checkout/CreateStripeCheckoutSessionTest.php
@@ -2,19 +2,62 @@
 
 namespace Tests\Feature\Actions\Checkout;
 
+use App\Actions\Checkout\CreateStripeCheckoutSession;
+use App\DataTransferObjects\CartItem;
+use App\Models\ProductPrice;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Mockery\MockInterface;
+use Stripe\Checkout\Session;
 use Tests\TestCase;
 
+/**
+ * @covers \App\Actions\Checkout\CreateStripeCheckoutSession
+ */
 class CreateStripeCheckoutSessionTest extends TestCase
 {
     /**
-     * A basic feature test example.
+     * @covers \App\Actions\Checkout\CreateStripeCheckoutSession::execute()
      */
-    public function test_example(): void
+    public function testCanExecute(): void
     {
-        $response = $this->get('/');
+        /** @var CreateStripeCheckoutSession&MockInterface $action */
+        $action = $this->partialMock(CreateStripeCheckoutSession::class);
+        $action->shouldAllowMockingProtectedMethods();
 
-        $response->assertStatus(200);
+        $user = new User();
+        $cartItems = [new CartItem(price: new ProductPrice())];
+
+        $stripeSession = new Session('stripe-session-1');
+        $stripeSession->url = 'https://stripe.com/checkout';
+
+        $action->expects('createStripeCheckoutSession')
+            ->once()
+            ->with($user, $cartItems)
+            ->andReturn($stripeSession);
+
+        $action->expects('createLocalSession')
+            ->once()
+            ->with('stripe-session-1', $user, $cartItems)
+            ->andReturn('https://stripe.com/checkout');
+
+        $this->assertSame('https://stripe.com/checkout', $action->execute($user, $cartItems));
+    }
+
+    /**
+     * @covers \App\Actions\Checkout\CreateStripeCheckoutSession::makeStripeLineItem()
+     */
+    public function testCanMakeStripeLineItem(): void
+    {
+        $this->markTestIncomplete(__METHOD__);
+    }
+
+    /**
+     * @covers \App\Actions\Checkout\CreateStripeCheckoutSession::createLocalSession()
+     */
+    public function testCanCreateLocalSession(): void
+    {
+        $this->markTestIncomplete(__METHOD__);
     }
 }
