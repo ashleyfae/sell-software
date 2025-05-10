9b55b62da741a402e12c5b40d411a8bb1707058a
Ashley Gibson <ashley@nosegraze.com>
2025-05-10 10:28:45 +0100

    Extract to new variable first.

--- app/Observers/BundleObserver.php
+++ app/Observers/BundleObserver.php
@@ -9,7 +9,10 @@ class BundleObserver
     public function saving(Bundle $bundle): void
     {
         if (! empty($bundle->price_ids) && is_array($bundle->price_ids)) {
-            sort($bundle->price_ids);
+            // this new variable is necessary because Laravel won't allow indirect modification of a property
+            $priceIds = $bundle->price_ids;
+            sort($priceIds);
+            $bundle->price_ids = $priceIds;
         }
     }
 }
